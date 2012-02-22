0bb8c45a5cfc5349be8e1afa1b900c58eddbbd0d
Andrew Rowson <github@growse.com>
2012-02-22 16:53:47 +0000

    Extensibility maximum!

--- thumbnailgenerator.py
+++ thumbnailgenerator.py
@@ -4,6 +4,8 @@ from os.path import splitext
 import subprocess
 import os.path
 
+number_of_posterfiles=4
+
 ftp = FTP('ftp.streaming.thomsonreuters2.netdna-cdn.com')
 ftp_username = 'streaming.thomsonreuters2'
 ftp_password = 'Gadget55'
@@ -15,20 +17,24 @@ for file in files:
 	print file
 	filename,extension = splitext(file)
 	if extension == '.mp4':
-		print "File is a video"
-		poster = filename+'_1.jpg'
+		print "File is an mp4 video"
+		poster = file+'_0.jpg'
 		if poster not in files:
 			print "Poster doesn't exist"
 			if not os.path.exists(file):
+				print "Downloading file"
 				ftp.retrbinary('RETR '+file,open(file, 'wb').write)
-			print "File download complete"
+				print "File download complete"
 			lengthout = subprocess.Popen(["ffmpeg","-i",file],stderr=subprocess.PIPE).communicate()[1].split()
 			duration = lengthout[lengthout.index('Duration:')+1].rstrip(',')
 			durations=duration.split(":")
 			totallength = int((int(durations[0])*3600)+(int(durations[1])*60)+float(durations[2]))
 			print "Total video length is: "+str(totallength)+"s"
-			interval = totallength / 5;
-			intervals = [interval,interval*2,interval*3,interval*4]
+
+			interval = totallength / (number_of_posterfiles+1);
+			intervals = []
+			for x in range(number_of_posterfiles)
+				intervals.append(interval*(x+1))
 			for idx,val in enumerate(intervals):
 				posterfile = file+"_"+str(idx)+".jpg"
 				print "Generating "+posterfile
