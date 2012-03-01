e20fd4677968783ac7abceced58014fd3b9d20ce
Andrew Rowson <github@growse.com>
2012-03-01 16:35:25 +0000

    Made the damn thing work

--- thumbnailgenerator.py
+++ thumbnailgenerator.py
@@ -1,3 +1,4 @@
+import shutil
 import math
 from ftplib import FTP
 from os.path import splitext
@@ -6,10 +7,10 @@ import os
 
 number_of_posterfiles=8
 thumbnail_dimension='160x90'
-thumbnail_quality=50
+thumbnail_quality=75
 directory='/mnt/s3fs/'
 posterfiledir='/mnt/s3fs/posterfiles/'
-
+tempdir = '/tmp/'
 files=os.listdir(directory)
 
 for file in files:
@@ -17,23 +18,27 @@ for file in files:
 	print "On S3: "+file
 	filename,extension = splitext(file)
 	if extension in ['.mp4','.m4v','.mov','.mkv']:
-		print "File is a video"
-		lengthout = subprocess.Popen(["ffmpeg","-i",directory+file],stderr=subprocess.PIPE).communicate()[1].split()
+		print "File is a video. Copying to "+tempdir+file
+		shutil.copy2(directory+file,tempdir+file)
+		print "Finding length:" 
+		lengthout = subprocess.Popen(['ffmpeg','-i',tempdir+file],stderr=subprocess.PIPE).communicate()[1].split()
 		print lengthout
 		duration = lengthout[lengthout.index('Duration:')+1].rstrip(',')
 		durations=duration.split(":")
 		totallength = int((int(durations[0])*3600)+(int(durations[1])*60)+float(durations[2]))
 		print "Total video length is: "+str(totallength)+"s"
 
-		interval = totallength / (number_of_posterfiles+1);
+		interval = float(totallength) / (number_of_posterfiles+1);
+		print "Interval is: "+str(interval)
 		intervals = []
 		for x in range(number_of_posterfiles):
 			intervals.append(interval*(x+1))
 		for idx,val in enumerate(intervals):
+			print "Posterfile "+str(idx)+" value "+str(val)
 			posterfile = posterfiledir+file+"_"+str(idx)+".jpg"
 			thumbnail_posterfile = posterfiledir+file+"_"+str(idx)+".th.jpg"
 			print "Generating "+posterfile
-			cmd = ["ffmpeg","-i",directory+file,"-an","-ss",str(val),"-f","mjpeg","-qmin","0.8","-qmax","0.8","-t","1","-r","1","-y",posterfile]
+			cmd = ["ffmpeg","-i",tempdir+file,"-an","-ss",str(val),"-f","mjpeg","-qmin","0.8","-qmax","0.8","-t","1","-r","1","-y",posterfile]
 			print cmd
 			outputs = subprocess.Popen(cmd,stderr=subprocess.PIPE).communicate()[1]
 			print outputs
