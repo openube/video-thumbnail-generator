38974821e0ca0d2d25c330a43010e570cb7e54ec
Andrew Rowson <github@growse.com>
2012-03-19 16:07:19 +0000

    Made CGI kickoff script actually work

--- kickoff-cgi.py
+++ kickoff-cgi.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python
 
+import pika
 import cgi
 import cgitb; cgitb.enable()
+
 print "Content-type: application/json"
-print
-print "hi there"
+qs = cgi.FieldStorage()
+
+if "name" not in qs:
+	print "Status: 404 Not Found"
+	print
+	print "{'404 Not Found'}"
+else:
+	connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+	channel = connection.channel()
+	channel.basic_publish(exchange='',routing_key='thumbnailgenerator',body=qs['name'].value)
+	print
+	print "{'"+str(qs["name"].value)+"'}"
+
