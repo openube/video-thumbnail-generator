0a47b845e6ec114a0849082bc871ccbd5967e912
Andrew Rowson <github@growse.com>
2012-03-21 13:22:23 +0000

    Moved from S3FS to boto

--- thumbnailgenerator.py
+++ thumbnailgenerator.py
@@ -4,38 +4,37 @@ import ftputil
 import stat
 import re
 import signal
-import shutil
-import math
-from os.path import splitext
 import datetime
 import simplejson as json
 import subprocess
 import os
-from progress_bar import ProgressBar
 import sys
 import pika
-import argparse
+import boto
 
 sigint_caught=False
 debug_mode=True
 number_of_posterfiles=8
 thumbnail_dimension='160x90'
 thumbnail_quality=75
-directory='/mnt/s3fs/'
-#directory='/tmp/test/'
-posterfiledir='/mnt/s3fs/posterfiles/'
-#posterfiledir='/tmp/test/posterfiles/'
 pidfile = "/tmp/thumbnailgenerator.pid"
 
-ftp_host = 'ftp.streaming.thomsonreuters2.netdna-cdn.com'
-ftp_username = 'streaming.thomsonreuters2'
-ftp_password = 'Gadget55'
+ftp_host = ''
+ftp_username = ''
+ftp_password = ''
 
-metafile = posterfiledir+'meta.js'
+metafile = 'posterfiles/meta.js'
 tempdir = '/tmp/'
-files=os.listdir(directory)
+bucket = {}
+s3_bucket_name = ''
 
 meta = {}
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
+
 
 def signal_handler(signal, frame):
 	global sigint_caught
@@ -53,7 +52,9 @@ def signal_handler(signal, frame):
 
 def main():
 	global meta
+	global bucket
 	print "Starting up"
+
 
 	#Exit on SIGINT
 	signal.signal(signal.SIGINT, signal_handler)
@@ -66,17 +67,14 @@ def main():
 	else:
 		file(pidfile, 'w').write(pid)
 
+	bucket = boto.connect_s3().get_bucket(s3_bucket_name)
+
 	debug("Opening rMQ connection")
 	connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 	channel = connection.channel()
 	channel.queue_declare(queue='thumbnailgenerator')
 	
-
-	#Load whatever metadata already exists
-	if os.path.exists(metafile):
-		f = open(metafile,'r')
-		meta = json.load(f)
-		f.close()
+	load_metadata()
 
 	#Start listening for messages:
 	channel.basic_consume(process_msg,queue='thumbnailgenerator',no_ack=True)
@@ -93,22 +91,31 @@ def process_msg(ch,method,properties,body):
 		if 'command' in decoded_msg:
 			if decoded_msg['command'] == 'add':
 				filename = decoded_msg['filename']
-				if (os.path.exists(directory+filename)):
+				if not bucket.get_key(filename) == None and bucket.get_key(filename).exists():
 					meta[filename] = get_metadata(filename)
 					generate_posterfiles(filename)
 					upload_to_ftp(filename)
 					commit_metadata()
 				else:
 					debug(filename + " doesn't seem to exist")
-			elif decoded_msg['command'] == 'removemetadata':
-				debug("Purging extraneous metadata")		
+			elif decoded_msg['command'] == 'purgemetadata':
+				debug("Purging extraneous metadata")
+				meta_to_purge = []
+				bucket_key_list = []
+				for key in bucket.list():
+					bucket_key_list.append(key.name)
+				debug("Got all keys. Size ="+str(len(bucket_key_list)))
 				for metafile in meta:
-					if not os.path.exists(directory+metafile):
-						meta.pop(metafile)
+					debug("seeing if "+metafile+" is absent")
+					if not metafile in bucket_key_list:
+						debug("File absent. Purging metadata for "+metafile)
+						meta_to_purge.append(metafile)
+				for metafile in meta_to_purge:
+					meta.pop(metafile)
 				commit_metadata()
 			elif decoded_msg['command'] == 'purgeftp':
 				debug("Purging extraneous FTP Files ")
-				##TODO: implement this
+
 				pass
 			else:
 				debug("Message not understood")
@@ -126,9 +133,19 @@ def process_msg(ch,method,properties,body):
 def commit_metadata():
 	debug("Writing metadata to disk")
 	#Commit the metadata back to disk
-	f = open(metafile,'w')
-	json.dump(meta,f)
-	f.close()
+	metakey = bucket.get_key(metafile)
+	if metakey==None:
+		metakey = bucket.new_key(metafile)
+	metakey.set_contents_from_string(json.dumps(meta))
+
+def load_metadata():
+	global meta
+	debug("Loading metadata from disk")
+	#Load whatever metadata already exists
+	metakey = bucket.get_key(metafile)
+	if not metakey == None and metakey.exists():
+		metastring = metakey.get_contents_as_string()
+		meta = json.loads(metastring)
 
 def generate_posterfiles(filename):
 	global meta
@@ -139,7 +156,7 @@ def generate_posterfiles(filename):
 	totallength = int((int(durations[0])*3600)+(int(durations[1])*60)+float(durations[2]))
 	debug("Copying file to tmp")
 	#Dump the video in a tempdirectory to reduce latency
-	shutil.copy2(directory+filename,tempdir+filename)
+	bucket.get_key(filename).get_contents_to_filename(tempdir+filename)
 	
 	#Set the permissions through chmod
 	os.chmod(tempdir+filename,stat.S_IRUSR)
@@ -150,13 +167,17 @@ def generate_posterfiles(filename):
 	for x in range(number_of_posterfiles):
 		intervals.append(interval*(x+1))
 	for idx,val in enumerate(intervals):
-		posterfile = posterfiledir + filename + "_"+str(idx)+".jpg"
-		thumbnail_posterfile = posterfiledir + filename + "_" + str(idx) + ".th.jpg"
+		posterfile = filename + "_"+str(idx)+".jpg"
+		thumbnail_posterfile = filename + "_" + str(idx) + ".th.jpg"
 		debug("Generating "+posterfile)
-		cmd = ["ffmpeg","-i",tempdir+filename,"-an","-ss",str(val),"-f","mjpeg","-qmin","0.8","-qmax","0.8","-t","1","-r","1","-y",posterfile]
-		outputs = subprocess.Popen(cmd,stderr=subprocess.PIPE).communicate()[1]
-		th_cmd = ["convert",posterfile,"-resize",thumbnail_dimension+"^","-gravity","center","-extent",thumbnail_dimension,"-quality",str(thumbnail_quality),thumbnail_posterfile]
-		th_outputs = subprocess.Popen(th_cmd,stderr=subprocess.PIPE).communicate()[1]
+		cmd = ["ffmpeg","-i",tempdir+filename,"-an","-ss",str(val),"-f","mjpeg","-qmin","0.8","-qmax","0.8","-t","1","-r","1","-y",tempdir+posterfile]
+		subprocess.Popen(cmd,stderr=subprocess.PIPE).communicate()[1]
+		debug("Generating "+thumbnail_posterfile)
+		th_cmd = ["convert",tempdir+posterfile,"-resize",thumbnail_dimension+"^","-gravity","center","-extent",thumbnail_dimension,"-quality",str(thumbnail_quality),tempdir+thumbnail_posterfile]
+		th_out = subprocess.Popen(th_cmd,stderr=subprocess.PIPE).communicate()[1]
+		debug("Uploading "+posterfile)
+		bucket.new_key('posterfiles/'+posterfile).set_contents_from_filename(tempdir+posterfile)
+		bucket.new_key('posterfiles/'+thumbnail_posterfile).set_contents_from_filename(tempdir+thumbnail_posterfile)
 	os.remove(tempdir+filename)
 
 ftp_upload_filesize=0
@@ -166,11 +187,14 @@ def upload_to_ftp(short_filename):
 	"""Expects short filename"""
 	global ftp_upload_progress
 	global ftp_upload_filesize
-	debug("Uploading to FTP")
-	ftp_upload_filesize=os.path.getsize(directory+short_filename)
+	debug("Caching file from S3")
+	filekey = bucket.get_key(short_filename)
+	ftp_upload_filesize = filekey.size
 	ftp_upload_progress=0
+	filekey.get_contents_to_filename(tempdir+short_filename)
+	debug("Uploading to FTP")
 	host = ftputil.FTPHost(ftp_host,ftp_username,ftp_password)
-	host.upload(directory+short_filename,short_filename,mode='b',callback=ftpcallback)
+	host.upload(tempdir+short_filename,short_filename,mode='b',callback=ftpcallback)
 	host.close()
 
 def ftpcallback(chunk):
@@ -184,13 +208,13 @@ def ftpcallback(chunk):
 def get_metadata(short_filename):
 	global meta
 	"""Expects short filename"""
-	filename = directory + short_filename
+	filekey = bucket.get_key(short_filename)
+	filename = tempdir+short_filename
+	filekey.get_contents_to_filename(filename)
 	debug("Extracting metadata from " +short_filename)
 	#Grab file info from ffmpeg
 	metadata_str = subprocess.Popen(['ffmpeg','-i',filename],stderr=subprocess.PIPE).communicate()[1]
 	metadata_parts = re.findall(r'[^,\|\n]+',metadata_str.replace(': ','|'))
-	metadata_start_index = 0
-	inc=0
 	metadata={}
 	val=''
 	seen_v_stream=0
